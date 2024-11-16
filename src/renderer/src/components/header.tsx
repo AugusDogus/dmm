f849b6cf526ba9ec8bc3f0b9c77c913ceda32804
AugusDogus <9794679+AugusDogus@users.noreply.github.com>
2024-11-15 20:49:11 -0600

    fix: header max width

--- src/renderer/src/components/header.tsx
+++ src/renderer/src/components/header.tsx
@@ -1,7 +1,7 @@
 import { Button } from '@renderer/components/ui/button';
 
 export function Header() {
-    return <header className='flex items-center px-4 pt-4 w-full'>
+    return <header className='flex items-center px-4 pt-4 w-full max-w-7xl'>
         <h1 className='w-full text-2xl font-bold'>Deadlock Mod Manager</h1>
         <div className="flex gap-2 justify-end w-full">
             <Button variant="outline">Patch</Button>
